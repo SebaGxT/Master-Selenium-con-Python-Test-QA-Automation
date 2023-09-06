af6bc753a9a2875b00761c8fd460bd4619d8b6bd
SebastianDelaVega <61508227+SebaGxT@users.noreply.github.com>
2023-09-06 18:57:56 -0300

    Modificaciones en practica 134-25

--- Seccion 25 - PYTEST/134-25-FixtureDecoratorsP1.py
+++ Seccion 25 - PYTEST/134-25-FixtureDecoratorsP1.py
@@ -2,22 +2,22 @@ import pytest
 
 @pytest.fixture(scope='module')
 def setup_login_uno():
-    print("\nEmpezando el login del sistema uno\n")
+    print("\n\nEmpezando el login del sistema uno\n")
     yield
-    print("\nSaliendo del sistema prueba ok")
+    print("\n\nSaliendo del sistema prueba ok\n")
 
 @pytest.fixture(scope='module')
 def setup_login_dos():
-    print("\nEmpezando las pruebas del sistema dos")
+    print("\n\nEmpezando las pruebas del sistema dos\n")
     yield
-    print("\nFin de las pruebas del sistema dos")
+    print("\n\nFin de las pruebas del sistema dos\n")
 
 def test_uno(setup_login_uno):
-    print("\nEmpezando las pruebas del test uno")
+    print("\n##### Empezando las pruebas del test uno #####\n")
 
 def test_dos(setup_login_dos):
-    print("\nEmpezando las pruebas del test dos")
+    print("\n@@@@@ Empezando las pruebas del test dos @@@@@\n")
 
 @pytest.mark.usefixtures("setup_login_dos")
 def test_tres():
-    print("\nPrueba tres del modulo login dos")
+    print("\n\nPrueba tres del modulo login dos\n\n")
